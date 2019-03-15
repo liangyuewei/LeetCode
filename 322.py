a5b8df8139e582f9b80980a98490b39576a46beb
yuewei.liang <yueweidev@gmail.com>
2019-03-15 13:07:57 +0800

    update 322.py

--- 322.py
+++ 322.py
@@ -1,9 +1,9 @@
 class Solution(object):
-    def coinChange(self,coins, amount):
-        dp = [anount + 1] * (amount + 1)
+    def coinChange(self, coins, amount):
+        dp = [amount + 1] * (amount + 1)
         dp[0] = 0
-        for i in range(amount):
+        for i in range(1, amount + 1):
             for j in range(0, len(coins)):
                 if coins[j] <= i:
-                    dp[i] = min(dp[i], dp[i - coins[j]]) + 1
-        return dp[amount] if dp[amount] > amount else -1
+                    dp[i] = min(dp[i], dp[i - coins[j]] + 1)
+        return -1 if dp[amount] > amount else dp[amount]
--- 72.py
+++ 72.py
@@ -9,4 +9,3 @@ class Solution(object):
             for j in range(1, n + 1):
                 dp[i][j] = dp[i-1][j-1] if word1[i-1] == word2[j-1] else min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
         return dp[m][n]
-
